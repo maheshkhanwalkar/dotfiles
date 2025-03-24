f178e5b0f5c5d3ada3856b9845b647a1ba016b33
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2025-03-23 21:11:22 -0400

    emacs: reorganize init.el into dependency and config sections

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,4 @@
-;; Set windmove to use M key
-;; I find this more convenient to use than Shift
-(windmove-default-keybindings 'meta)
+;; Dependencies ----------------------------------------------------------------
 
 ;; Install MELPA package manager
 (require 'package)
@@ -17,7 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company pdf-tools vscode-dark-plus-theme))
+ '(package-selected-packages nil)
  '(pdf-tools-handle-upgrades t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -31,10 +29,6 @@
   :ensure t)
 (add-hook 'after-init-hook 'global-company-mode)
 
-;; Use K&R C style, since it's the best
-(setq-default c-default-style
-	      '((c-mode . "k&r")))
-
 ;; pdf-tools allows for viewing PDFs in emacs itself
 (use-package pdf-tools
   :ensure t
@@ -44,3 +38,22 @@
 ;; magit for git integration
 (use-package magit
   :ensure t)
+
+;; Configuration ---------------------------------------------------------------
+
+;; Set windmove to use M key
+;; I find this more convenient to use than Shift
+(windmove-default-keybindings 'meta)
+
+;; Use K&R C style, since it's the best
+(setq-default c-default-style
+	      '((c-mode . "k&r")))
+
+;; Display line and column numbers
+(setq-default column-number-mode t)
+
+;; Backup directory, so we don't have random '~' files everywhere, but still
+;; are able to keep backups enabled
+(setq-default backup-directory-alist '(("." . "/tmp/emacs/backup")))
+
+;; -----------------------------------------------------------------------------
